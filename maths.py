d771931fbd9305e4ae19aaf6de8629682138f4d4
LunchboxMG <mikey53x@yahoo.com>
2018-04-10 16:26:03 -0500

    Added matrix operations.

--- maths.py
+++ maths.py
@@ -1,6 +1,9 @@
 import numpy as np
-from numpy import ndarray
+from numpy import ndarray, identity, zeros
 from numpy import sqrt as npsqrt, sum as npsum, dot
+
+from constants import *
+# Type Conversions
 
 FLOAT32 = np.float32
 UINT32 = np.uint32
@@ -145,6 +148,170 @@ class Vector3f(Vector2f):
 class Vector3i(Vector3f):
 
     _UNIT = UINT32
+
+# Matrix operations
+
+def translate(m, v):
+    """ Perform a translation on the input matrix `m` using `v` position
+    vector. """
+
+    r = np.copy(m)
+    r[3] = m[0] * v[0] + m[1] * v[1] + m[2] * v[2] + m[3]
+
+    return r
+
+def rotate(m, angle, v):
+    """ Rotate about the `v` axis by the `angle`.
+
+    NOTE: v is in degrees. """
+
+    c = cos(angle*D2R)
+    s = sin(angle*D2R)
+
+    axis = normalize(v)
+    temp = (1.0 - c) * axis
+
+    # Setup rotation matrix
+    rot00 = c + temp[0] * axis[0]
+    rot01 = temp[0] * axis[1] + s * axis[2]
+    rot02 = temp[0] * axis[2] - s * axis[1]
+
+    rot10 = temp[1] * axis[0] - s * axis[2]
+    rot11 = c + temp[1] * axis[1]
+    rot12 = temp[1] * axis[2] + s * axis[0]
+
+    rot20 = temp[2] * axis[0] + s * axis[1]
+    rot21 = temp[2] * axis[1] - s * axis[0]
+    rot22 = c + temp[2] * axis[2]
+
+    # Apply rotation
+    r = zeros((4, 4), dtype=FLOAT32)
+    r[0] = m[0] * rot00 + m[1] * rot01 + m[2] * rot02
+    r[1] = m[0] * rot10 + m[1] * rot11 + m[2] * rot12
+    r[2] = m[0] * rot20 + m[1] * rot21 + m[2] * rot22
+    r[3] = m[3]
+
+    return r
+
+def scale(m, v):
+    """ Apply scaling to the input matrix `m`. """
+
+    r = zeros((4, 4), dtype=FLOAT32)
+    r[0] = m[0] * v[0]
+    r[1] = m[1] * v[1]
+    r[2] = m[2] * v[2]
+    r[3] = m[3]
+
+    return r
+
+def lookAtRH(eye, center, up):
+    """ Camera view matrix utilizing the right hand coordinate system. """
+
+    f = normalize(center - eye)
+    s = normalize(cross(f, up))
+    u = cross(s, f)
+
+    r = identity(4, dtype=FLOAT32)
+    r[0:3, 0] = s
+    r[0:3, 1] = u
+    r[0:3, 2] = -f
+    r[3, 0] = -dot(s, eye)
+    r[3, 1] = -dot(u, eye)
+    r[3, 2] = dot(f, eye)
+
+    return r
+
+def lookAtLH(eye, center, up):
+    """ Camera view matrix utilizing the left hand coordinate system. """
+
+    f = normalize(center - eye)
+    s = normalize(cross(up, f))
+    u = cross(s, f)
+
+    r = identity(4, dtype=FLOAT32)
+    r[0:3, 0] = s
+    r[0:3, 1] = u
+    r[0:3, 2] = f
+    r[3, 0] = -dot(s, eye)
+    r[3, 1] = -dot(u, eye)
+    r[3, 2] = -dot(f, eye)
+
+    return r
+
+def perspectiveRH(fovy, aspect, znear, zfar):
+
+    fovy *= D2R
+    tan_half_fovy = tan(fovy * 0.5)
+
+    r = zeros((4,4), dtype=FLOAT32)
+    r[0, 0] = 1.0 / (aspect * tan_half_fovy)
+    r[1, 1] = 1.0 / (tan_half_fovy)
+    r[2, 3] = -1.0
+
+    # Option 1 (if GLM_CLIP_SPACE == GL_DEPTH_ZERO_TO_ONE)
+    r[2, 2] = zfar / (znear - zfar)
+    r[3, 2] = -(zfar * znear) / (zfar - znear)
+
+    # Option 2
+    #r[2, 2] = -(zfar + znear) / (zfar - znear)
+    #r[3, 2] = -2.0 * zfar * znear / (zfar - znear)
+
+    return r
+
+def perspectiveLH(fovy, aspect, znear, zfar):
+    """ Prespective projection for 3D rendering that utilizes the
+    left hand coordinate system. """
+
+    fovy *= D2R
+    tan_half_fovy = tan(fovy * 0.5)
+
+    r = zeros((4,4), dtype=FLOAT32)
+    r[0, 0] = 1.0 / (aspect * tan_half_fovy)
+    r[1, 1] = 1.0 / tan_half_fovy
+    r[2, 3] = 1.0
+
+    # Option 1 (if GLM_CLIP_SPACE == GL_DEPTH_ZERO_TO_ONE)
+    r[2, 2] = zfar / (zfar - znear)
+    r[3, 2] = -(zfar * znear) / (zfar - znear)
+
+    # Option 2
+    #r[2, 2] = (zfar + znear) / (zfar - znear)
+    #r[3, 2] = -2.0 * zfar * znear / (zfar - znear)
+
+    return r
+
+def orthoRH(left, right, bottom, top, znear, zfar):
+    """ Orthographic projection for 3D rendering that utilizes the
+    right hand coordinate system. """
+
+    r = identity(4, dtype=FLOAT32)
+
+    r[0, 0] = 2.0 / (right - left)
+    r[1, 1] = 2.0 / (top - bottom)
+    r[3, 0] = -(right + left) / (right - left)
+    r[3, 1] = -(top + bottom) / (top - bottom)
+
+    # Option 1 (if GLM_DEPTH_CLIP_SPACE == GLM_DEPTH_ZERO_TO_ONE)
+    r[2, 2] = -1.0 / (zfar - znear)
+    r[3, 2] = -znear / (zfar - znear)
+
+    # else
+    #r[2, 2] = -2.0 / (zfar - znear)
+    #r[3, 2] = -(zfar + znear) / (zfar - znear)
+
+    return r
+
+def ortho2D(left, right, bottom, top):
+    """ Orthographic projection for 2D rendering. """
+
+    r = identity(4, dtype=FLOAT32)
+    r[0, 0] = 2.0 / (right - left)
+    r[1, 1] = 2.0 / (top - bottom)
+    r[2, 2] = -1.0
+    r[3, 0] = -(right + left) / (right - left)
+    r[3, 1] = -(top + bottom) / (top - bottom)
+
+    return r
 
 # Math functions
 
