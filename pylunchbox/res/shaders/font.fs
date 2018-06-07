d5f56ee0a9b983bf95aab1191f28220037cb635c
LunchboxMG <mikey53x@yahoo.com>
2018-06-07 05:57:33 -0500

    font update

--- pylunchbox/res/shaders/font.fs
+++ pylunchbox/res/shaders/font.fs
@@ -6,9 +6,31 @@ out vec4 out_color;
 
 uniform sampler2D tex;
 uniform vec3 color;
+uniform vec3 outline_color
+
+// For large font, use low edge value, slightly higher width
+// For small font, use high edge value, slightly lower width
+const float width = 0.5;
+const float edge = 0.1;
+
+// for glow, increase edge, lower width
+const float border_width = 0.7; // set to zero for no outline
+const float border_edge = 0.1;
+
+// for shadow
+const vec2 offset(0.006, 0.006); // set to zero for no shadow
 
 void main(void) {
 
-    float alpha = texture(tex, pass_uv);
-    out_color = vec4(color, alpha);
+    // How far away from the outer edge of the distance field
+    float dist = 1.0f - texture(tex, pass_uv).a;
+    float alpha = 1.0f - smoothstep(width, width + edge, dist);
+
+    float dist2 = 1.0f - texture(tex, pass_uv + offset).a;
+    float alpha_outline = 1.0f - smoothstep(border_width, border_width + border_edge, dist2);
+
+    float alpha_total = alpha + (1.0 - alpha) * alpha_outline;
+    float color_total = mix(color, outline_color, alpha / alpha_total);
+
+    out_color = vec4(color_total, alpha_total);
 }
